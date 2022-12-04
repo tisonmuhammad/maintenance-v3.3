cb4455a36cd974de43d2114fe9be62fa3db8d2d8
Muhammad Tison <tisonmuhammad@gmail.com>
2022-12-05 00:18:06 +0700

    add class mt5

--- components/Bawah.jsx
+++ components/Bawah.jsx
@@ -22,7 +22,7 @@ export default function Bawah(props) {
             <div className="Container">
                 <div className="Row">
                     <div className={`${styles["kol0"]} ${styles["kolxxl3"]} ${styles["kolxl4"]} ${styles["kollg5"]} ${styles["kolmd6"]} ${styles["kolsMd8"]} ${styles["kolsm9"]} ${styles["kol11"]}`}>
-                        <div style="margin-top:50px;"></div>
+                        <div className='mt-5'></div>
                         {/* <Image className='logoFooter' src="/logo-fm-bg-white.png" alt="First Media Logo" width={62.75} height={40} /> */}
                         <hr className="line" />
                         <p className="textCopyright">
